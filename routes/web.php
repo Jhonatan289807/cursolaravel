a43153b37fc1acf29196033651219f4dcc12bcc7
jhonatan <7jhon2898@gmail.com>
2021-10-26 09:59:56 -0500

    primer video

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "Bienvenido a la pagina principal";
+});
+Route::get('cursos', function () {
+    return "Bienvenido a la pagina de cursos";
+});
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+});
+/*Route::get('cursos/{curso}',function($curso){
+    return "Bienvenido al curso de $curso";
+});*/
+Route::get('cursos/{curso}/{categoria?}', function ($curso,$categoria = null) { //poniendo el signo ? al final de categoria indicamos que es un valor opcional ya que si no se llegara a pasar un valor a categoria este se declara como null
+    if($categoria){
+        return "Bienvenido al curso $curso de la categoria $categoria";
+    }else{
+        return "Bienvenido al curso $curso";
+    }
 });
